a9a2225ab0a9f2893fc75ce60062fc0a3a1c265b
jstrmby <48553432+jstmrby@users.noreply.github.com>
2024-10-09 14:14:31 +0200

    define dap config

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,67 @@
+return {
+  "mfussenegger/nvim-dap",
+  opts = function()
+    local dap = require("dap")
+    if not dap.adapters["pwa-node"] then
+      dap.adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            LazyVim.get_pkg_path("js-debug-adapter", "/js-debug/src/dapDebugServer.js"),
+            "${port}",
+          },
+        },
+      }
+    end
+
+    if not dap.adapters["node"] then
+      dap.adapters["node"] = function(cb, config)
+        if config.type == "node" then
+          config.type = "pwa-node"
+        end
+        local nativeAdapter = dap.adapters["pwa-node"]
+        if type(nativeAdapter) == "function" then
+          nativeAdapter(cb, config)
+        else
+          cb(nativeAdapter)
+        end
+      end
+    end
+
+    local js_filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" }
+
+    for _, language in ipairs(js_filetypes) do
+      dap.configurations[language] = {
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach to port",
+          port = function()
+            return vim.fn.input("Port: ", "5856")
+          end,
+          cwd = "${workspaceFolder}",
+          skipFiles = { "<node_internals>/**" },
+        },
+        {
+          type = "pwa-node",
+          request = "attach",
+          name = "Attach to process",
+          processId = require("dap.utils").pick_process,
+          cwd = "${workspaceFolder}",
+          skipFiles = { "<node_internals>/**" },
+        },
+        {
+          type = "pwa-node",
+          request = "launch",
+          name = "Launch file",
+          program = "${file}",
+          cwd = "${workspaceFolder}",
+          skipFiles = { "<node_internals>/**" },
+        },
+      }
+    end
+  end,
+}
