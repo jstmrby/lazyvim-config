eb6a7e03d177823b404134e096aa46e1e5227acb
jstrmby <48553432+jstmrby@users.noreply.github.com>
2024-11-13 23:42:41 +0100

    change which-key custom mapping

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,8 +1,9 @@
 return {
   "folke/which-key.nvim",
   opts = {
-    defaults = {
-      ["<leader>h"] = { name = "+harpoon" },
+    spec = {
+      -- ["<leader>h"] = { name = "+harpoon" },
+      { "<leader>h", group = "harpoon" },
     },
   },
 }
