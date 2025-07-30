f73f3a3c82983cb8ba59fd189567423250ce87da
jstrmby <48553432+jstmrby@users.noreply.github.com>
2025-07-30 11:28:52 +0200

    customize lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,78 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  opts = function(_, opts)
+    local icons = LazyVim.config.icons
+
+    opts.sections.lualine_c = {
+      LazyVim.lualine.root_dir(),
+      {
+        "diagnostics",
+        symbols = {
+          error = icons.diagnostics.Error,
+          warn = icons.diagnostics.Warn,
+          info = icons.diagnostics.Info,
+          hint = icons.diagnostics.Hint,
+        },
+      },
+      { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+      { LazyVim.lualine.pretty_path({ length = 6 }) },
+    }
+
+    opts.sections.lualine_x = {
+      Snacks.profiler.status(),
+      {
+        function()
+          return require("noice").api.status.command.get()
+        end,
+        cond = function()
+          return package.loaded["noice"] and require("noice").api.status.command.has()
+        end,
+        color = function()
+          return { fg = Snacks.util.color("Statement") }
+        end,
+      },
+      {
+        function()
+          return require("noice").api.status.mode.get()
+        end,
+        cond = function()
+          return package.loaded["noice"] and require("noice").api.status.mode.has()
+        end,
+        color = function()
+          return { fg = Snacks.util.color("Constant") }
+        end,
+      },
+      {
+        function()
+          return "  " .. require("dap").status()
+        end,
+        cond = function()
+          return package.loaded["dap"] and require("dap").status() ~= ""
+        end,
+        color = function()
+          return { fg = Snacks.util.color("Debug") }
+        end,
+      },
+      {
+        "diff",
+        symbols = {
+          added = icons.git.added,
+          modified = icons.git.modified,
+          removed = icons.git.removed,
+        },
+        source = function()
+          local gitsigns = vim.b.gitsigns_status_dict
+          if gitsigns then
+            return {
+              added = gitsigns.added,
+              modified = gitsigns.changed,
+              removed = gitsigns.removed,
+            }
+          end
+        end,
+      },
+    }
+
+    return opts
+  end,
+}
