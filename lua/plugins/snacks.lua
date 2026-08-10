fcc4a0d0120cb9c336438ebaf9acf04a997d099a
jstrmby <48553432+jstmrby@users.noreply.github.com>
2026-08-10 16:26:20 +0200

    add preview horizontal scrolling

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -20,6 +20,20 @@ return {
           truncate = 80,
         },
       },
+      win = {
+        input = {
+          keys = {
+            ["<C-Left>"] = { "preview_scroll_left", mode = { "i", "n" } },
+            ["<C-Right>"] = { "preview_scroll_right", mode = { "i", "n" } },
+          },
+        },
+        list = {
+          keys = {
+            ["<C-Left>"] = "preview_scroll_left",
+            ["<C-Right>"] = "preview_scroll_right",
+          },
+        },
+      },
     },
   },
 }
